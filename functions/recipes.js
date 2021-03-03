fec463f476c9dd9815e1ae8bce1e7eb7d4831e9f
Michał Winiecki <falrond@gmail.com>
2021-03-03 18:50:09 +0100

    airtable view

--- functions/recipes.js
+++ functions/recipes.js
@@ -7,7 +7,7 @@ const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
 
 exports.handler = async (event, context) => {
   try {
-    const { records } = await airtable.list();
+    const { records } = await airtable.list({ view: 'Grid view' });
     const recipes = records.map(recipe => {
       const { id } = recipe;
       const { name, images, time, Date } = recipe.fields;
